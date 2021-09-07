90cec4060fc3dcf23f5153cd97e3c16e3a118353
JooJaeSeong <joojae26@gmail.com>
2021-09-07 22:37:56 +0900

    Fix & Docs : Banner 이미지 파일, 애니메이션 재생시간 수정

--- client/src/images.js
+++ client/src/images.js
@@ -1,34 +1,32 @@
 // Banner
-import Banner1 from "./images/main/banner/banner_1.png";
-import Banner2 from "./images/main/banner/banner_2.png";
-import Banner3 from "./images/main/banner/banner_3.png";
-import Banner4 from "./images/main/banner/banner_4.png";
+import Banner1 from './images/main/banner/banner_1.svg';
+import Banner2 from './images/main/banner/banner_2.svg';
+import Banner4 from './images/main/banner/banner_4.svg';
 
 // // Top
-import Sheffield from "./images/main/top/sheffield_univ.png";
-import Arkansas from "./images/main/top/arkansas_univ.png";
-import Lincoln from "./images/main/top/lincoln_univ.png";
+import Sheffield from './images/main/top/sheffield_univ.png';
+import Arkansas from './images/main/top/arkansas_univ.png';
+import Lincoln from './images/main/top/lincoln_univ.png';
 
 // // Middle
-import Imoge from "./images/main/middle/mac_man.svg";
-import Imac from "./images/main/middle/imac.svg";
+import Imoge from './images/main/middle/mac_man.svg';
+import Imac from './images/main/middle/imac.svg';
 
 // // Bottom
-import Iphone from "./images/main/bottom/Iphone.png";
-import { ReactComponent as MsgBox } from "./images/main/bottom/msgBox.svg";
-import { ReactComponent as Quotes } from "./images/main/bottom/quotes.svg";
+import Iphone from './images/main/bottom/Iphone.png';
+import { ReactComponent as MsgBox } from './images/main/bottom/msgBox.svg';
+import { ReactComponent as Quotes } from './images/main/bottom/quotes.svg';
 
 export {
-	Banner1,
-	Banner2,
-	Banner3,
-	Banner4,
-	Sheffield,
-	Arkansas,
-	Lincoln,
-	Imoge,
-	Imac,
-	Iphone,
-	MsgBox,
-	Quotes,
+  Banner1,
+  Banner2,
+  Banner4,
+  Sheffield,
+  Arkansas,
+  Lincoln,
+  Imoge,
+  Imac,
+  Iphone,
+  MsgBox,
+  Quotes,
 };
