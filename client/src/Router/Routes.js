e71a18e9e0dd6104080ea582431a9996aa3a5b5a
Createhb21 <alsghk9701@gmail.com>
2021-10-02 18:28:07 +0900

    change community -> CommunityEntrance

--- client/src/Router/Routes.js
+++ client/src/Router/Routes.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Home from '../Components/Layout/Home/Home';
-import Community from '../Pages/Community';
-import FreeBoard from "../Pages/FreeBoard";
-import QnABoard from "../Pages/QnABoard";
-import Result from "../Pages/Result";
+import CommunityEntrance from '../Pages/CommunityEntrance';
+import FreeBoard from '../Pages/FreeBoard';
+import QnABoard from '../Pages/QnABoard';
+import Result from '../Pages/Result';
 
 class Routes extends React.Component {
   render() {
     return (
       <HashRouter>
-          <>
-            <Switch>
-              <Route path="/" exact component={Home}/>
-              <Route path="/result/:koUniv" exact component={Result}/>
-              <Route path="/result/:koUniv/:continent" exact component={Result}/>
-              <Route path="/result/:koUniv/:continent/:country" component={Result}/>
-              <Route path="/community/:continent" exact component={Community}/>
-              <Route path="/community/:continent/freeboard" exact component={FreeBoard}/>
-              <Route path="/community/:continent/qna" exact component={QnABoard}/>
-              <Redirect from="*" to="/"/>
-            </Switch>
-          </>
+        <>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/result/:koUniv" exact component={Result} />
+            <Route path="/result/:koUniv/:continent" exact component={Result} />
+            <Route
+              path="/result/:koUniv/:continent/:country"
+              component={Result}
+            />
+            <Route
+              path="/community/:continent"
+              exact
+              component={CommunityEntrance}
+            />
+            <Route
+              path="/community/:continent/freeboard"
+              exact
+              component={FreeBoard}
+            />
+            <Route
+              path="/community/:continent/qna"
+              exact
+              component={QnABoard}
+            />
+            <Redirect from="*" to="/" />
+          </Switch>
+        </>
       </HashRouter>
     );
   }
